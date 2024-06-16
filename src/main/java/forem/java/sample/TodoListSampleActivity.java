474d52c1fe4e18f3fcf658f036329c47aa70d5eb
megamiel <hrydan4779@gmail.com>
2024-06-16 10:48:51 +0900

    added sample program

--- src/main/java/forem/java/sample/TodoListSampleActivity.java
+++ src/main/java/forem/java/sample/TodoListSampleActivity.java
@@ -9,61 +9,64 @@ import forem.java.extensions.ForemList;
 import forem.java.ui.ForemListView;
 import forem.java.views.HorizontalLayout;
 import forem.java.views.Root;
+import forem.java.views.VarArea;
 
 public class TodoListSampleActivity extends ForemActivity {
     @Override
     public void render(Root root) {
+        onBack = none;
         var todoList = var(new ForemList<String>());
-        var taskEditText = varType(EditText.class);
-        space(match_parent, 0, 1);
-        this.<HorizontalLayout>create(() -> {
+        var todoEditText = varType(EditText.class);
+        $.<HorizontalLayout>create(() -> {
             layout(match_parent, 0, 10);
         }).render(() -> {
-            this.<EditText>create(taskEditText, () -> {
+            $.<EditText>create(todoEditText, () -> {
                 layout(0, match_parent, 70);
                 hint("タスクを入力");
                 textSize(24);
             });
-            this.<Button>create(() -> {
+            $.<Button>create(() -> {
                 layout(0, match_parent, 30);
                 text("追加");
                 textSize(24);
                 onClick(() -> {
-                    var taskName = get(taskEditText).getText().toString();
-                    if (taskName.equals(""))
-                        return;
-                    get(taskEditText).setText("");
-                    get(todoList).add(taskName);
-//                    ignite(todoList);
+                    var todo = todoEditText[v].getText().toString();
+                    if (todo.equals("") || todoList[v].contains(todo)) return;
+                    text("aiueo");
+                    todoEditText[v].setText("");
+                    todoList[v].add(todo);
+                    notify(todoList);
                 });
             });
         });
         ForemListView.create(() -> {
-            layout(match_parent, 0, 89);
+            layout(match_parent, 0, 90);
         }).render(() -> {
-//            onChange(todoList, () -> {
-//                clearChild();
-//                get(todoList).forEach((todo, index) -> {
-//                    this.<HorizontalLayout>create(() -> {
-//                        layout(match_parent, wrap_content);
-//                    }).render(() -> {
-//                        this.<TextView>create(() -> {
-//                            layout(0, match_parent, 60);
-//                            text(todo);
-//                            textSize(26);
-//                        });
-//                        this.<Button>create(() -> {
-//                            layout(0, match_parent, 40);
-//                            text("完了");
-//                            textSize(18);
-//                            onClick(() -> {
-//                                get(todoList).remove(index);
-//                                ignite(todoList);
-//                            });
-//                        });
-//                    });
-//                });
-//            });
+            $.<VarArea>create(() -> {
+                layout(match_parent);
+                onNotify(todoList, () -> {
+                    todoList[v].forEach((todo, i) -> {
+                        $.<HorizontalLayout>create(() -> {
+                            layout(match_parent, wrap_content);
+                        }).render(() -> {
+                            $.<TextView>create(() -> {
+                                layout(0, match_parent, 70);
+                                text(todo);
+                                textSize(22);
+                            });
+                            $.<Button>create(() -> {
+                                layout(0, match_parent, 30);
+                                text("完了");
+                                textSize(22);
+                                onClick(() -> {
+                                    todoList[v].remove(i);
+                                    notify(todoList);
+                                });
+                            });
+                        });
+                    });
+                });
+            });
         });
     }
 }
